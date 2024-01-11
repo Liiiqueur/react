39548fb28c4b91e22f6a5bb2d2b209236c2c0bdf
Liqueur <Liiiiiiiqueur.gmail.com>
2024-01-12 00:46:49 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(props) {//컨포넌트
+  console.log('props', props, props.title);
+  return <header> 
+    <h1><a href="/" onClick={(event)=>{
+      event.preventDefault(); //a태그의 기본동작을 방지함으로써 클릭 시 리로드 방지
+      props.onChangeMode();
+    }}>{props.title}</a></h1>
+  </header>
+}
+function Nav(props) {
+  const lis = []
+  for(let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key = {t.id}><a id={t.id} href={'/read/'+t.id} onClick={event=>{
+      event.preventDefault();
+      props.onChangeMode(event.target.id);
+    }}>{t.title}</a></li>)
+  }
+  return <nav>
+    <ol>
+      {lis}
+    </ol>
+  </nav>
+}
+function Article(props) {
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
 function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is...'},
+    {id:2, title:'css', body:'css is...'},
+    {id:3, title:'js', body:'js is...'}
+  ]
   return (
     <div>
-      <header>
-        <h1><a href="/">WEB</a></h1>
-      </header>
+        <Header title="WEB" onChangeMode={()=>{
+          alert('Header');
+          }}></Header>
+        <Nav topics={topics} onChangeMode={(id)=>{
+          alert(id);
+        }}></Nav>
+        <Article title="Welcome" body="Hello, WEB"></Article>
     </div>
   );
 }
